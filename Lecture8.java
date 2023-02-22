c98a9498c993293d44ad21db445c2692dd8571ed
ajong003 <ajong003@fiu.edu>
2023-02-22 17:14:53 -0500

    Update Lecture8.java

--- Lecture8.java
+++ Lecture8.java
@@ -11,7 +11,6 @@ class Node
  
 class Lecture8
 {
-    // Recursive function to check if a given binary tree is a sum tree or not
     public static int isSumTree(Node root)
     {
         if(root==null){
@@ -32,10 +31,38 @@ class Lecture8
         }
 
 
- 
+
 
     }
- 
+    //book solution
+    // Recursive function to check if a given binary tree is a sum tree or not
+    public static int isSumTree2(Node root)
+    {
+        // base case: empty tree
+        if (root == null) {
+            return 0;
+        }
+
+        // special case: leaf node
+        if (root.left == null && root.right == null) {
+            return root.key;
+        }
+
+        int left = isSumTree2(root.left);
+        int right = isSumTree2(root.right);
+
+        // if the root's value is equal to the sum of all elements present in its
+        // left and right subtree
+        if (left != Integer.MIN_VALUE && right != Integer.MIN_VALUE &&
+                root.key == left + right) {
+            return 2 * root.key;
+        }
+
+        return Integer.MIN_VALUE;
+    }
+
+    // Recursive function to check if a given binary tree is a sum tree or not
+
     public static void main(String[] args)
     {
         /* Construct the following tree
